17e0f36dcfe5b7e2a231c4c0b8a120037c190ee5
Sneha Srivastava <srivastavasneha895@gmail.com>
2025-10-07 21:23:19 +0530

    Add files via upload

---
+++ Day 1_Basics.py
@@ -0,0 +1,7 @@
+# 1. Hello world
+print("Hello World !")
+
+# 2. Input and Output
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(f" Hello {name}, you are {age} years old")
